3fa49eedcf3b12a16ebb57b27df3de437804e09f
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-19 18:12:24 +0300

    Updated perusterveys case

--- src/App/components/Image/index.js
+++ src/App/components/Image/index.js
@@ -7,6 +7,7 @@ import cs from 'classnames'
 export default class extends Component {
   state = {
     loaded: false,
+    thumbHidden: false,
   }
   mounted = true
 
@@ -15,7 +16,11 @@ export default class extends Component {
     const loader = new Image()
     loader.onload = loader.onerror = () => {
       // setTimeout(() => {
-        if (this.mounted) this.setState({ loaded: true })
+        if (this.mounted) this.setState({ loaded: true }, () => {
+          setTimeout(() => {
+            if (this.mounted) this.setState({ thumbHidden: true })
+          }, 1000)
+        })
       // }, 1000)
     }
     loader.src = src
@@ -26,7 +31,7 @@ export default class extends Component {
   }
 
   render() {
-    const { loaded } = this.state
+    const { loaded, thumbHidden } = this.state
     const { className, thumbSrc, src, ...rest } = this.props
 
     return (
@@ -34,10 +39,11 @@ export default class extends Component {
         'Image',
         thumbSrc && 'Image--hasThumb',
         loaded && 'Image--loaded',
+        thumbHidden && 'Image--thumbHidden',
         className
       )}>
         { thumbSrc && (
-          <img { ...rest } src={ thumbSrc } />
+          <img { ...rest } src={ thumbSrc } className="Image__thumb" />
         )}
         { (loaded || !thumbSrc) && (
           <img { ...rest } src={ src } className="Image__main" />
--- src/App/contents/cases/PerusTerveys/index.js
+++ src/App/contents/cases/PerusTerveys/index.js
@@ -10,6 +10,8 @@ import Summary from 'App/components/Summary'
 
 import heroThumb from './perusterveys-hero-thumb.jpg'
 import hero from './perusterveys-hero.png'
+import photoThumb from './perusterveys-photo-thumb.jpg'
+import photo from './perusterveys-photo.png'
 import summary from './perusterveys-summary.svg'
 
 export default () => (
@@ -24,7 +26,43 @@ export default () => (
     <SubTitle useAsMeta>Feeling better about booking online</SubTitle>
     <hr />
     <Keywords>Full-stack development, Service design</Keywords>
-    <Image thumbSrc={ heroThumb } src={ hero } alt="PerusTerveys" />
+    <Image thumbSrc={ heroThumb } src={ hero } alt="PerusTerveys app screenshot" />
+    <Chapter>
+      <h4>Setting the scene</h4>
+      <p>
+        PerusTerveys is revolutionizing the Finnish healthcare services
+        industry with general practitioner and laboratory services at
+        affordable prices and without long waiting times. The positive
+        public response has led to rapid growth – the business has expanded
+        from a handful of medical centers to a national chain of private
+        clinics.
+      </p>
+    </Chapter>
+    <Chapter>
+      <h4>The bright idea</h4>
+      <p>
+        What if private healthcare services could be made accessible for
+        everyone?
+      </p>
+      <p>
+        To do this, the online booking system should be as user friendly and
+        easy to use as possible – in fact, it should be the best booking
+        system in the world.
+      </p>
+    </Chapter>
+    <Chapter>
+      <h4>The inevitable challenge</h4>
+      <p>
+        According to customer feedback, PerusTerveys’s current online booking
+        system was far too complicated to use – so much so that most of
+        their customers ended up booking appointments by phone instead.
+        With no developer or service designer expertise in-house,
+        PerusTerveys turned to Wunderdog to handle the entire execution of
+        their new online booking system, from service design to full stack
+        development.
+      </p>
+    </Chapter>
+    <Image thumbSrc={ photoThumb } src={ photo } alt="Crew photo" />
     <Chapter>
       <h4>Our simple yet smart solution</h4>
       <p>
