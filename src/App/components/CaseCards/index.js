c45e365ace3d65534e7c9fa7aaf97f56507b62df
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-19 22:44:26 +0300

    Not nudging case cards if user scrolls

--- src/App/components/CaseCards/index.js
+++ src/App/components/CaseCards/index.js
@@ -8,12 +8,57 @@ import Card from 'App/components/Card'
 
 export default withState(
   class extends Component {
+    state = {
+      nudgeCancelled: false,
+      nudged: false,
+    }
+    nudgeTimer = null
+
+    componentDidUpdate() {
+      this.nudgeMaybe()
+    }
+
+    componentDidMount() {
+      window.addEventListener('scroll', this.cancelNudge)
+      this.nudgeMaybe()
+    }
+
+    componentWillUnmount() {
+      this.cancelNudge()
+    }
+
+    nudgeMaybe() {
+      if (
+        !this.state.nudged &&
+        !this.nudgeTimer &&
+        !this.props.splashShowing
+      ) {
+        this.nudgeTimer = setTimeout(() => {
+          this.setState({ nudged: true })
+          this.clearNudgeTimerAndListener()
+        }, 2000)
+      }
+    }
+
+    cancelNudge = () => {
+      this.setState({ nudgeCancelled: true })
+      this.clearNudgeTimerAndListener()
+    }
+
+    clearNudgeTimerAndListener() {
+      window.removeEventListener('scroll', this.cancelNudge)
+      clearTimeout(this.nudgeTimer)
+      this.nudgeTimer = null
+    }
+
     render() {
       const { splashShowing } = this.props
+      const { nudged, nudgeCancelled } = this.state
       return(
-        <div className={
-          cs('CaseCards', !splashShowing && 'CaseCards--showContent')
-        }>
+        <div className={cs(
+          'CaseCards',
+          nudged && !nudgeCancelled && 'CaseCards--nudged',
+        )}>
           <div className="CaseCards__inner">
             <div>
             <Card to="/case/perusterveys">
