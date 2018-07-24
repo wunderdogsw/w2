7a4cbf3a9e1d969c3549e87eef6389bba1e55d50
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-24 17:59:30 +0300

    Tuned footer and header

--- src/App/contents/callToActions/LetsTalk/index.js
+++ src/App/contents/callToActions/LetsTalk/index.js
@@ -5,6 +5,6 @@ export default () => (
   <Fragment>
     <h1>Let's talk!</h1>
     <p>Find various ways to contact us from our contact page</p>
-    <Button to="/contact" useAccent reversed>Contact</Button>
+    <Button to="/contact">Contact</Button>
   </Fragment>
 )
