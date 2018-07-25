3d45352d78b8e46d94be3b220b58b18b6e18defc
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2018-07-25 14:50:04 +0300

    Uusi case

--- src/App/contents/pages/index.js
+++ src/App/contents/pages/index.js
@@ -5,6 +5,7 @@ import importedComponent from 'react-imported-component'
 
 export const Career = importedComponent(() => import('./Career'))
 export const Contact = importedComponent(() => import('./Contact'))
+export const FiudenDemo = importedComponent(() => import('./FiudenDemo'))
 export const PerusTerveys = importedComponent(() => import('./PerusTerveys'))
 export const Pexraytech = importedComponent(() => import('./Pexraytech'))
 export const RedBull = importedComponent(() => import('./RedBull'))
