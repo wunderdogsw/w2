f66d050cae0d2be7cd91d3d52e92395cbc7b2c83
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-26 14:06:07 +0300

    added address tags

--- src/App/contents/contactOffices/Helsinki/index.js
+++ src/App/contents/contactOffices/Helsinki/index.js
@@ -4,13 +4,13 @@ import TransitionLink from 'App/components/TransitionLink'
 export default () => (
   <Fragment>
     <h4>Helsinki</h4>
-    <p>
+    <address>
       Mikonkatu 13 A, 4 krs.<br />
       00100 Helsinki, Finland<br />
       <TransitionLink
         to="https://www.google.fi/maps/place/Mikonkatu+13,+00100+Helsinki"
         children="Map"
       />
-    </p>
+    </address>
   </Fragment>
 )
