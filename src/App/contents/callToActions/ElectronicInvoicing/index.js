d26ddcaf263b1b8fc398a0f74dc44d685828c2ed
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-31 12:54:21 +0300

    Updated electronic invoicing layout

--- src/App/contents/callToActions/ElectronicInvoicing/index.js
+++ src/App/contents/callToActions/ElectronicInvoicing/index.js
@@ -1,14 +1,37 @@
 import React, { Fragment } from 'react'
-import Button from 'App/components/Button'
+import ChapterRow from 'App/components/ChapterRow'
 
 export default () => (
   <Fragment>
     <h1>Electronic Invoicing</h1>
-    <p>
-      Wunderdog Oy (2617416-4)<br />
-      Basware Oyj<br />
-      003726174164<br />
-      BAWCFI22
-    </p>
+    <ChapterRow>
+      <div>
+        <h5>Finland</h5>
+        <p>
+          Wunderdog Oy (2617416-4)<br />
+          Basware Oyj<br />
+          003726174164<br />
+          BAWCFI22
+        </p>
+      </div>
+      <div>
+        <h5>Germany</h5>
+        <p>
+          Wunderdog Oy (2617416-4)<br />
+          Basware Oyj<br />
+          003726174164<br />
+          BAWCFI22
+        </p>
+      </div>
+      <div>
+        <h5>Sweden</h5>
+        <p>
+          Wunderdog Oy (2617416-4)<br />
+          Basware Oyj<br />
+          003726174164<br />
+          BAWCFI22
+        </p>
+      </div>
+    </ChapterRow>
   </Fragment>
 )
