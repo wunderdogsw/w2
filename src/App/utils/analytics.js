d83faa89663733c7d780082ae7a06fc5c90f40d8
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-31 12:02:00 +0300

    Tuned analytics

--- src/App/utils/analytics.js
+++ src/App/utils/analytics.js
@@ -17,5 +17,11 @@ export const pageview = url => {
 
 export const event = ga.event
 export const outboundLink = e => {
+
+  // React ga thinks that links with @ are email adresses and might ignore them
+  // Medium links may contain @ characters sooooooo...
+  // replace
+  if (e.label) e.label = e.label.replace('@', '(at)')
+
   ga.outboundLink(e, noop)
 }
