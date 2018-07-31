9ca4acdf18656eb6dd9a35d562268dd81c4f8090
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-07-31 13:17:05 +0300

    Added support for Stats on mobile devices

--- src/App/components/Stats/index.js
+++ src/App/components/Stats/index.js
@@ -1,9 +1,41 @@
 import './index.css'
-import React from 'react'
+import React, { Component, createRef } from 'react'
 import cs from 'classnames'
 
-export default ({ children, useAccent }) => (
-  <section className={cs('Stats', useAccent && 'Stats--useAccent')}>
-    <ul>{ children }</ul>
-  </section>
-)
+
+export default class extends Component {
+  state = {
+    current: 0,
+  }
+  timer = null
+  ref = createRef()
+
+  componentDidMount() {
+    this.timer = setInterval(this.change, 2000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  change = () => {
+    const ref = this.ref.current
+    let next = this.state.current + 1
+    if (next >= ref.children.length) next = 0
+    this.setState({ current: next })
+  }
+
+  render() {
+    const { children, useAccent } = this.props
+    const { current } = this.state
+    return (
+      <section className={cs(
+        'Stats',
+        useAccent && 'Stats--useAccent',
+        `Stats--${ current }`
+      )}>
+        <ul ref={ this.ref }>{ children }</ul>
+      </section>
+    )
+  }
+}
