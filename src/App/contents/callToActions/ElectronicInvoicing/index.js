5881245cf4e29c77275995079bdb9666caffcec1
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-08-01 16:20:05 +0300

    Reset electronic invoicing content

--- src/App/contents/callToActions/ElectronicInvoicing/index.js
+++ src/App/contents/callToActions/ElectronicInvoicing/index.js
@@ -1,37 +1,13 @@
 import React, { Fragment } from 'react'
-import ChapterRow from 'App/components/ChapterRow'
 
 export default () => (
   <Fragment>
     <h1>Electronic Invoicing</h1>
-    <ChapterRow>
-      <div>
-        <h5>Finland</h5>
-        <p>
-          Wunderdog Oy (2617416-4)<br />
-          Basware Oyj<br />
-          003726174164<br />
-          BAWCFI22
-        </p>
-      </div>
-      <div>
-        <h5>Germany</h5>
-        <p>
-          Wunderdog Oy (2617416-4)<br />
-          Basware Oyj<br />
-          003726174164<br />
-          BAWCFI22
-        </p>
-      </div>
-      <div>
-        <h5>Sweden</h5>
-        <p>
-          Wunderdog Oy (2617416-4)<br />
-          Basware Oyj<br />
-          003726174164<br />
-          BAWCFI22
-        </p>
-      </div>
-    </ChapterRow>
+    <p>
+      Wunderdog Oy (2617416-4)<br />
+      Basware Oyj<br />
+      003726174164<br />
+      BAWCFI22
+    </p>
   </Fragment>
 )
