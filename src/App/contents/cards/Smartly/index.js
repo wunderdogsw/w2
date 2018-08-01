f196d150d2481ffaf0412de4f6f8a6798385252e
Viljami <viljami.anttonen@gmail.com>
2018-08-01 18:29:48 +0300

    Smartly was missing that .io

--- src/App/contents/cards/Smartly/index.js
+++ src/App/contents/cards/Smartly/index.js
@@ -8,6 +8,6 @@ export default () => (
   <Fragment>
     <Image thumbSrc={ thumb } src={ picture } alt="Smartly" />
     <h4>Teamwork makes the dream work</h4>
-    <p>Smartly</p>
+    <p>Smartly.io</p>
   </Fragment>
 )
