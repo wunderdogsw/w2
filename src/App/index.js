73174098f429789902d154c624b6a85143385546
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-08-02 16:07:22 +0300

    Updated redirects

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { State } from 'App/state'
 import Splash from 'App/components/Splash'
 import Header from 'App/components/Header'
@@ -12,9 +12,48 @@ import { FooterBottom } from 'App/contents/other'
 export default () => (
   <BrowserRouter>
     <State>
+
+      {/* External redirects */}
+      <Route path="*" render={({ match }) => {
+        switch (match.url) {
+          case '/instagram':
+          case '/instagram-1':
+            window.location = 'https://www.instagram.com/wunderdogsw'
+            break
+
+          case '/kulttuurimme':
+            window.location = 'https://medium.com/wunderdog-culture'
+            break
+        }
+        return null
+      }}/>
+
       <Switch>
+
+        {/* Internal redirects */}
+        <Redirect from="/wunderdog-fi" to="/" />
+        <Redirect from="/en" to="/" />
+        <Redirect from="/main" to="/" />
+        <Redirect from="/wundernuts" to="/" />
+        <Redirect from="/wunderkuutio" to="/" />
+        <Redirect from="/wundernuts-1" to="/" />
+        <Redirect from="/we-do-wunders" to="/aboutus" />
+        <Redirect from="/wedowunders-fi" to="/aboutus" />
+        <Redirect from="/fullstacker-to-our-lund-pack" to="/career" />
+        <Redirect from="/fullstack-developer-berlin-germany" to="/career" />
+        <Redirect from="/fullstack-developer-lund" to="/career" />
+        <Redirect from="/join-the-pack" to="/career" />
+        <Redirect from="/we-are-hiring-join-the-pack" to="/career" />
+        <Redirect from="/liity-joukkoon" to="/career" />
+        <Redirect from="/fullstack-developer" to="/career" />
+        <Redirect from="/interaction-designer" to="/career" />
+        <Redirect from="/ota-yhteytta" to="/contact" />
+        <Redirect from="/contact-us" to="/contact" />
+
+        {/* Routes */}
         <Route path="/:page" component={ Page } />
         <Route path="*" component={ Home } />
+
       </Switch>
       <Footer>
         <Switch>
