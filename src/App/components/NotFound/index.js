cd5dbc21477ddb5527fc01df2125b95520049d04
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-08-03 12:03:12 +0300

    Tracking 404 with ga

--- src/App/components/NotFound/index.js
+++ src/App/components/NotFound/index.js
@@ -1,9 +1,26 @@
 import './index.css'
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { NotFound as Content } from 'App/contents/other'
+import { event } from 'App/utils/analytics'
 
-export default () => (
-  <div className="NotFound">
-    <Content />
-  </div>
+
+export default withRouter(
+  class extends Component {
+    componentDidMount() {
+      const { match } = this.props
+      event({
+        category: 'Page not found',
+        action: match.url,
+      })
+    }
+
+    render() {
+      return (
+        <div className="NotFound">
+          <Content />
+        </div>
+      )
+    }
+  }
 )
