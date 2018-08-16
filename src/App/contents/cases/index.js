4cda0dd794da96799907a42f1e823de615e650cc
vilmaranki <vilma.ranki@wunderdog.fi>
2018-08-16 10:53:19 +0300

    Edited home page text

---
+++ src/App/contents/cases/index.js
@@ -0,0 +1,7 @@
+// Do not modify by hand
+// File generated with `mapContent` script
+
+import importedComponent from 'react-imported-component'
+
+export const Pexraytech = importedComponent(() => import('./Pexraytech'))
+export const Smartly = importedComponent(() => import('./Smartly'))
