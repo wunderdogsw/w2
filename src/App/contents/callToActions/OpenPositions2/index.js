f16ef2bb20fb0ba82334e45edf159882997ca7a9
Viljami <viljami.anttonen@gmail.com>
2018-09-17 13:35:06 +0300

    Updated wunderdnut copy

--- src/App/contents/callToActions/OpenPositions2/index.js
+++ src/App/contents/callToActions/OpenPositions2/index.js
@@ -5,8 +5,7 @@ export default () => (
   <Fragment>
     <h1>Wundernut</h1>
     <p>
-      Our latest coding challenge, Wundernut Back to School, is running.
-      Send your solution by September 30. Check the nut @ GitHub.
+      Participate to our latest coding challenge in github!
     </p>
     <Button to="https://github.com/wunderdogsw/wunderpahkina-vol9" reversed>
       Crack the nut
