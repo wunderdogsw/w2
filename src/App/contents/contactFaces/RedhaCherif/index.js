2e000f00dce4aaf6a1ce1754357f8a9c3f93ffcf
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2019-01-15 15:46:52 +0200

    redha info update

--- src/App/contents/contactFaces/RedhaCherif/index.js
+++ src/App/contents/contactFaces/RedhaCherif/index.js
@@ -6,10 +6,6 @@ export default () => (
     <p>
       Country Lead
       <br />
-      <a className="breakAll noHyphen" href="tel:+46761442771">
-        +46 (0) 761 442 771
-      </a>
-      <br />
       <a className="noHyphen" href="mailto:redha.cherif@wunder.dog">
         redha.cherif<span>@</span>wunder.dog
       </a>
