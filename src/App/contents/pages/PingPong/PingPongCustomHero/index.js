cbbd3ac3b35d391c03302aefb1e102b7e892405b
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2019-03-05 13:14:16 +0200

    updates for pingpong page

--- src/App/contents/pages/PingPong/PingPongCustomHero/index.js
+++ src/App/contents/pages/PingPong/PingPongCustomHero/index.js
@@ -2,30 +2,13 @@ import './index.css'
 import React, { Component } from 'react'
 import MainTitle from 'App/components/MainTitle'
 
-export default class extends Component {
-  name = false
-
-  componentDidMount() {
-    this.name = location.search
-
-    console.log(this.name)
-  }
-
-  render() {
-    return (
-      <div className="PingPongCustomHero">
-        <PingPongLogo/>
-        <PingPongBall/>
-        <MainTitle centered>You have been challenged</MainTitle>
-      </div>
-    )
-  }
-}
-
-
-
-
-
+export default () => (
+  <div className="PingPongCustomHero">
+    <PingPongLogo/>
+    <PingPongBall/>
+    <MainTitle centered>You have been challenged</MainTitle>
+  </div>
+)
 
 const PingPongLogo = () => (
   <svg className="PingPongLogo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 85">
@@ -48,8 +31,6 @@ const PingPongLogo = () => (
     <path className="tournament" d="M113.4 70c-0.3-0.4-0.1-0.7 0.4-0.6 0.9 0 1.8 0 2.7-0.1 1-1.8 1.9-3.3 2.4-4 0.3-0.4 0.6-0.4 0.8 0 0.4 0.9 0.4 2-0.4 3.6 1.3 0 1.7 0.5 2 1 0.2 0.4 0.1 0.6-0.4 0.6 -0.7-0.1-1.7 0-2.5 0.1 -1.2 2.1-3.1 5.3-4.3 7.9 -1.1 2.3-0.7 4.3 0.6 4.4 1.4 0.1 3.3-2.2 4.5-4.6 0.2-0.4 0.5-0.4 0.7 0 0.2 0.5 0 1.1-0.3 1.8 -0.4 0.9-2.5 3.7-5.1 3.7 -2.1-0.1-3.8-2.4-2.3-6.1 0.7-1.6 2-4.3 3.4-6.8C114.4 70.9 113.7 70.4 113.4 70z"/>
   </svg>
 )
-
-
 
 const PingPongBall = () => (
   <svg className="PingPongBall" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
