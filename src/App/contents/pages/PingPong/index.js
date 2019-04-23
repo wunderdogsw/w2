146af1ad72b1ebad422fa45bf24399ab7bc443d4
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2019-04-23 14:58:01 +0300

    updates-pingpong page

--- src/App/contents/pages/PingPong/index.js
+++ src/App/contents/pages/PingPong/index.js
@@ -24,33 +24,44 @@ export default () => (
     <PingPongCustomHero />
 
     <SubTitle alt narrow>
-      All the tech companies in Helsinki, this one’s for you!<br/><br/>
-      Our passion for table tennis is real, and we’ve heard rumours that there are other ping pong lovers too. We are organizing a very first Table Tennis Tournament between the tech companies in Helsinki area to clear up who really owns the game. At least in 2019! So pick your team, sharpen your strategy and be ready for the battle!
+      Our passion for table tennis is real, and we’ve heard rumours that there
+      are other ping pong lovers too. We are organizing a very first Table Tennis
+      Tournament between the tech companies in Helsinki to clear up who
+      owns the game. At least in 2019! So pick your team, sharpen your
+      strategy and be ready for the battle!
     </SubTitle>
 
     <Chapter>
       <h4>Tournament rules</h4>
       <p>
-        The Tournament will be played in teams. In each team, there shall be 4 players.
+        The Tournament will be played in teams. In each team, there shall be
+        3 players.
       </p>
       <p>
-        The points of every individual game shall be added together and the teams are ranked by the team score.
+        The points of every individual game shall be added together and the
+        teams are ranked by the team score.
       </p>
       <p>
-        The tournament consists of qualification rounds, quarterfinals, semifinals and the final.
+        The tournament consists of qualification rounds, quarterfinals,
+        semifinals and the final. We have 4 table tennis tables to make the
+        tournament smooth.
       </p>
       <p>
-        The team members shall be employees of the same company.
+        The team members shall be employees of the same company and only one
+        team per company will accepted.
       </p>
     </Chapter>
 
     <Chapter>
       <h4>General information</h4>
       <p>
-        You should register here before [[ INSERT LAST DAY TO SIGN IN HERE ]].
+        You should register here before 7 May 2019.
       </p>
       <p>
-        The tournament takes place at [[ INSERT DAY OF TOURNAMENT ]]. The location of the tournament is at Wuffice headquarters: Mikonkatu 13 A, 00100 Helsinki. After the Tournament we will have the Sauna ready for you! Snacks & refreshments are provided.
+        The tournament takes place on Thursday 17 May at 16.00. The
+        location of the tournament is at Wuffice HQ: Mikonkatu 13 A,
+        00100 Helsinki. After the Tournament, we will have the Sauna ready for
+        you! Snacks & refreshments are provided.
       </p>
     </Chapter>
 
