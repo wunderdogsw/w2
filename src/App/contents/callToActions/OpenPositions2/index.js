20f596d7e3a7895648b696c75e80aa8d94d09ced
vilma <36898281+vilmaranki@users.noreply.github.com>
2019-06-06 12:58:18 +0300

    Added open positions component to front page (#95)

--- src/App/contents/callToActions/OpenPositions2/index.js
+++ src/App/contents/callToActions/OpenPositions2/index.js
@@ -1,13 +1,23 @@
 import React, { Fragment } from 'react'
 import Button from 'App/components/Button'
+import TransitionLink from 'App/components/TransitionLink'
 
 export default () => (
   <Fragment>
-    <h1>Wundernut</h1>
+    <h1>Open positions</h1>
     <p>
-      Our latest coding challenge is out! Check it, and crack the nut by 2 June!</p>
-    <Button to="https://wunder.dog/pahkina" reversed>
-      Crack the nut
+      <TransitionLink to="https://www.recright.com/careers/en/wunderdog/full-stack-developer-5b6445f07c244b0001364709">
+        Full-stack developer, Lund
+      </TransitionLink><br/>
+      <TransitionLink to="https://www.recright.com/careers/en/wunderdog/full-stack-developer-5b6308472954ea0001a7ef5b">
+        Full-stack developer, Helsinki
+      </TransitionLink><br/>
+      <TransitionLink to= "https://www.recright.com/careers/en/wunderdog/full-stack-developer-5b6446f27c244b00013647d4">
+        Full-stack developer, Berlin
+      </TransitionLink><br/>
+    </p>
+    <Button to="https://www.recright.com/careers/en/wunderdog" reversed>
+      All Positions
     </Button>
   </Fragment>
 )
