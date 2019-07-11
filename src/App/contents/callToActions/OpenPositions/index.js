926a6365e2711fcd5138ec3c2a8f10a17a76440f
vilma <36898281+vilmaranki@users.noreply.github.com>
2019-07-11 10:51:06 +0300

    edited open positions block (#102)

--- src/App/contents/callToActions/OpenPositions/index.js
+++ src/App/contents/callToActions/OpenPositions/index.js
@@ -4,13 +4,20 @@ import TransitionLink from 'App/components/TransitionLink'
 
 export default () => (
   <Fragment>
-    <h1>Join us</h1>
+    <h1>Open positions</h1>
     <p>
-    Unique people make us who we are.<br/>
-    We are now hiring new personalities to join our pack.
+      <TransitionLink to="https://wunderdog.breezy.hr/p/9aa01a0d81cd-fullstack-developer-helsinki-finland">
+        Full-stack developer, Helsinki
+      </TransitionLink><br/>
+      <TransitionLink to="https://wunderdog.breezy.hr/p/4512f54c796a-fullstack-developer-d-f-m--berlin--germany">
+        Full-stack developer, Berlin
+      </TransitionLink><br/>
+      <TransitionLink to= "https://wunderdog.breezy.hr/p/9fdd4744af8f-fullstack-developer-malm--sweden">
+        Full-stack developer, Malmö
+      </TransitionLink><br/>
     </p>
-    <Button to="https://weare.wunder.dog/join-us" reversed>
-      View more
+    <Button to="https://wunderdog.breezy.hr/" reversed>
+      All Positions
     </Button>
   </Fragment>
 )
