78abea702109d0f374e204c86a239b3c96ed0ea3
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2019-10-09 15:07:04 +0300

    Mieli-casestory-building

---
+++ src/App/contents/blog/index.js
@@ -0,0 +1,27 @@
+// Do not modify by hand
+// File generated with `mapContent` script
+
+import importedComponent from 'react-imported-component'
+
+export const AgileMethods = importedComponent(() => import('./AgileMethods'))
+export const AreWeCeosEveryone = importedComponent(() => import('./AreWeCeosEveryone'))
+export const BabyItsColdOutside = importedComponent(() => import('./BabyItsColdOutside'))
+export const DontPlayItAgain = importedComponent(() => import('./DontPlayItAgain'))
+export const Hank = importedComponent(() => import('./Hank'))
+export const Hunttis = importedComponent(() => import('./Hunttis'))
+export const ImportanceOfCommunicationsInProductDevelopment = importedComponent(() => import('./ImportanceOfCommunicationsInProductDevelopment'))
+export const ImprovingYourProduct = importedComponent(() => import('./ImprovingYourProduct'))
+export const InvisibleSoftware = importedComponent(() => import('./InvisibleSoftware'))
+export const ItsTheWritingThatsWrong = importedComponent(() => import('./ItsTheWritingThatsWrong'))
+export const Kiti = importedComponent(() => import('./Kiti'))
+export const Monitoring = importedComponent(() => import('./Monitoring'))
+export const NorthStarMetric = importedComponent(() => import('./NorthStarMetric'))
+export const Nukka = importedComponent(() => import('./Nukka'))
+export const OpenToGoodIdeas = importedComponent(() => import('./OpenToGoodIdeas'))
+export const PairProgramming = importedComponent(() => import('./PairProgramming'))
+export const PairProgrammingPartTwo = importedComponent(() => import('./PairProgrammingPartTwo'))
+export const Piispa = importedComponent(() => import('./Piispa'))
+export const Pomo = importedComponent(() => import('./Pomo'))
+export const TechnicalDebtThreatOrPossibility = importedComponent(() => import('./TechnicalDebtThreatOrPossibility'))
+export const Turger = importedComponent(() => import('./Turger'))
+export const WhatsYourNameAgain = importedComponent(() => import('./WhatsYourNameAgain'))
