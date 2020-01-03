7cca61c24724f0af1c8048d6077a29fb4967b42f
Fiudenaattori <anna-maria.lafiura@wunderdog.fi>
2020-01-03 16:05:52 +0200

    content updates

--- src/App/contents/callToActions/OpenPositions/index.js
+++ src/App/contents/callToActions/OpenPositions/index.js
@@ -7,13 +7,25 @@ import TransitionLink from 'App/components/TransitionLink'
 export default () => (
   <Fragment>
     <h2>Join the pack</h2>
+    <TransitionLink className="TransitionLink--list" to="https://wunderdog.breezy.hr/p/685af3eaf0c2-backend-developer-consultant">
+      <span>Backend developer</span>
+      <span class="grey">Helsinki & Berlin</span>
+    </TransitionLink>
+    <TransitionLink className="TransitionLink--list" to="https://wunderdog.breezy.hr/p/07ed55ef20f3-frontend-developer-consultant-helsinki">
+      <span>Frontend developer</span>
+      <span class="grey">Helsinki & Berlin</span>
+    </TransitionLink>
     <TransitionLink className="TransitionLink--list" to="https://wunderdog.breezy.hr/p/9aa01a0d81cd-fullstack-developer-helsinki-finland">
       <span>Full-stack developer</span>
-      <span class="grey">Helsinki, Finland</span>
+      <span class="grey">Helsinki</span>
     </TransitionLink>
     <TransitionLink className="TransitionLink--list" to="https://wunderdog.breezy.hr/p/4512f54c796a-fullstack-developer-d-f-m--berlin--germany">
       <span>Full-stack developer</span>
-      <span class="grey">Berlin, Germany</span>
+      <span class="grey">Berlin</span>
+    </TransitionLink>
+    <TransitionLink className="TransitionLink--list" to="https://wunderdog.breezy.hr/p/5cd5b2850f13-open-applications">
+      <span>Open application</span>
+      <span class="grey">Helsinki & Berlin</span>
     </TransitionLink>
   </Fragment>
 )
