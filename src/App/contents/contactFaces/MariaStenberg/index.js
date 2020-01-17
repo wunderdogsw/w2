ad77b62b4bfaa47230f4f618f0c74555358c8293
isla-hash <maria.stenberg@wunderdog.fi>
2020-01-17 11:08:45 +0200

    contact-info-update-2

---
+++ src/App/contents/contactFaces/MariaStenberg/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+export default () => (
+  <Fragment>
+    <h5>Maria Stenberg</h5>
+    <p>
+      Marketing & Media
+      <br />
+      +358 44 4918387
+      <br />
+      <a className="noHyphen" href="mailto:maria.stenberg@wunderdog.fi">
+        maria.stenberg<span>@</span>wunderdog.fi
+      </a>
+    </p>
+  </Fragment>
+)
