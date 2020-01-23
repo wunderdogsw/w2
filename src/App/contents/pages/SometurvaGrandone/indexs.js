e0b2a6a8a03a9ce66ceebd8814da67ba25e2d4d2
isla-hash <maria.stenberg@wunderdog.fi>
2020-01-23 12:51:43 +0200

    somebuddy-grandone-update

--- src/App/contents/pages/SometurvaGrandone/indexs.js
+++ src/App/contents/pages/SometurvaGrandone/indexs.js
@@ -2,7 +2,7 @@ import importedComponent from 'react-imported-component'
 import image from './someturva-hero.jpg'
 
 export default {
-  title: 'Help for online harassment and cyberbullying',
+  title: 'Delivering justice for young people online',
   client: 'Somebuddy',
   publishDate: 'November 14, 2019',
   keywords: 'React, UX Design',
