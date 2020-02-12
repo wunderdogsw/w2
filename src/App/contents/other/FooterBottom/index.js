8fd87416c5bdecec9b7664837bfbd6c23eff78a7
vilmaranki <vilma.ranki@wunderdog.fi>
2020-02-12 14:15:01 +0200

    added automatic date instead of hardcoded

--- src/App/contents/other/FooterBottom/index.js
+++ src/App/contents/other/FooterBottom/index.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react'
 import FooterBottom from 'App/components/FooterBottom'
 import Logo from 'App/components/Logo'
 import TransitionLink from 'App/components/TransitionLink'
+var d = new Date();
 
 export default () => (
   <Fragment>
     <FooterBottom>
-      <p>© 2019 Wunderdog. WUNDERDOG is a registered trademark of Wunderdog Oy in the European Union.</p>
+      <p>© {d.getFullYear()} Wunderdog. WUNDERDOG is a registered trademark of Wunderdog Oy 
+      in the European Union.</p>
       <nav>
         <TransitionLink to="/cookies">Cookies Policy</TransitionLink>
         <TransitionLink to="/privacy">Privacy Policy</TransitionLink>
