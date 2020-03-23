676a187a976d4b3059c1eaae481e3381b6dbc221
vilmaranki <vilma.ranki@wunderdog.fi>
2020-03-23 07:36:06 +0200

    Corrected the grammar

--- src/App/contents/blog/AnAssetCalledCultureToTakeYouThroughTheHardTimes/index.js
+++ src/App/contents/blog/AnAssetCalledCultureToTakeYouThroughTheHardTimes/index.js
@@ -2,16 +2,16 @@ import importedComponent from 'react-imported-component'
 import image from './Seiskavisa-Wunderdog-office.jpg'
 
 export default {
-  title: 'Home office day 7: an asset called culture to take you through the hard times',
+  title: 'An Asset Called Culture to Carry You Through Hard Times',
   author: {
     name: 'Wunderdog',
     title: 'Wunderdog'
   },
-  publishDate: 'March 20, 2020',
+  publishDate: 'March 23, 2020',
   readTimeMinutes: 7,
   keywords: 'culture, remote work',
   category: 'culture',
-  image: image, 
+  image: image,
 
   content: importedComponent(() => import('./post')),
 }
--- src/App/contents/blog/AnAssetCalledCultureToTakeYouThroughTheHardTimes/post.js
+++ src/App/contents/blog/AnAssetCalledCultureToTakeYouThroughTheHardTimes/post.js
@@ -16,64 +16,67 @@ export default () => (
   <Fragment>
       <Chapter>
       <p>
-        During the past week, the Corona Virus has filled our news feeds and
+        During the past week, the coronavirus has filled our news feeds and
         caused worry, insecurity, and anxiety. We at Wunderdog take the official
-        instructions and recommendations seriously and do our part in minimizing
+        instructions and recommendations seriously and are doing our part in minimizing
         an exponential spreading of the virus – the situation demands
         collaborative actions as well as individual participation. Despite the
-        situation, we continue working hard on solving the problems our
-        customers face. And we do so by staying connected and optimistic. The
-        collaborative culture still lies at the heart of everything we do, even
+        situation, we continue to work hard on solving the problems our
+        customers face. And we do so by staying connected and optimistic. A culture
+        of collaboration still lies at the heart of everything we do, even
         - and especially - when working remotely.
         </p>
         <p>
         We’ve had to get used to working remotely and not seeing each other on
-        a daily basis. In addition to actual work, the team building activities
-        are now relying on distant connections. We began working from home
-        offices last Thursday. Adapting to remote conditions began right away.
-        Google Hangouts was proved to come in handy for not only the official
-        meetings but also for company-wide 2 o’clock coffee break, planking
-        session and yet another habit of ours: weekly Seiska magazine quiz.
+        a daily basis. In addition to our daily client work, the team building activities
+        now rely on remote connectivity. We began working from home
+        offices last Thursday, adapting to remote conditions right away.
+        Google Hangouts proved to come in handy for not only the official
+        meetings but also for more informal events such as our company-wide two
+        o’clock coffee break, planking session, and another quirky habit of ours, our
+        weekly Seiska magazine quiz.
         </p>
         <Image src={photo1} alt="Seiskavisa-Wunderdog-office1"/>
         <p>
-        It’s been a long week, every day bringing new news. Situations change
-        extremely fast. Transparent and accurate communication is required to
-        keep everyone on the loop. But what’s also crucial, is keeping up the
-        good spirit and having a laugh together during these difficult times.
-        The smallest of things might have a great impact on the general
-        atmosphere that affects everyone’s wellbeing and endurance during this
-        period of special arrangements. Being actively connected, having
+        Last week was a long week, every day bringing new news. The situation is
+        changing extremely fast, thus transparent and accurate communication is required to
+        keep everyone in the loop. Nonetheless it is crucial to keep up good
+        spirits and have a laugh together during these difficult times.
+        Even the smallest of things might have a large impact on the general
+        atmosphere, which could affect everyone’s wellbeing and endurance during
+        this period of special circumstances. Being actively connected, having
         Hangouts facilitated coffee breaks and sticking to weekly routines -
-        even from distance - can make a difference.
+        even remotely - can make all the difference.
       </p>
       <Image src={photo2} alt='Slack-Wunderdog-coffeebreak' />
       <p>
-        Taking the required distance now, means staying closer in spirit. Taking
-        coffee breaks via Hangouts, learning about our shared love for Iittala’s
-        Taika mugs, sharing creative home office solutions, crafting toilet
-        paper emojis or just sharing openly our feelings and reaching for
-        peer-to-peer support have been exceptional ways to keep our team spirit
-        together and uplifted. Our distance is paradoxically bringing us closer.
+        Keeping the required distance now means staying closer in spirit;
+        communal breaks over Hangouts, learning about our shared love for
+        Iittala’s Taika mugs, sharing creative home office solutions, designing
+        toilet paper emojis or just talking openly about our feelings and relying
+        on peer-to-peer support have been exceptional ways to keep our team spirit
+        united and uplifted. Our distance is paradoxically bringing us closer.
         It has shown how powerful a community can be.
       </p>
         <Image src={photo3} alt='remote-work-wunderdog' />
       <p>
-        The most important thing to do now is to stay safe and healthy. We’re
-        also aware the economic effects of the situation might affect our
-        customers and therefore us too. However, we believe staying positive
-        during unexpected circumstances is key. Being united during these times
-        is one way we prepare for what’s to come. The culture that has been
-        built together in better times gets truly tested in the harder ones.
-        In times like this the culture can be the force that takes us far -
-        it’s important to keep contributing to that.
+        The most important thing to do right now is to stay safe and healthy.
+        We are aware of the economic effects of the situation and how it might
+        affect our customers’ business and, consequently, our business too.
+        However, we believe staying positive during trying circumstances such as
+        these is key. Continuing to stay united during these times is one way we
+        can continue to be prepared for what is to come. The culture that we have
+        built together in better times gets truly tested in harder ones. In times
+        like these the culture can be a force that takes us far - it’s important
+        to keep contributing to that.
       </p>
       <p>
-        Here is our challenge to you: can you think of one - just one - thing
-        that you can do already today in your workplace to uplift the spirit
-        and connect with your colleagues? You are free to recycle our ideas.
-        If you come up with something new, please share them to us for example
-        on Instagram at @wunderdogsw. Let’s win the COVID-19 together!
+        Here is our challenge to you: can you think of one thing, big or small,
+        that you can already do today in your workplace to uplift spirits and
+        connect with your colleagues? Please feel free to recycle our ideas and,
+        if you come up with something new, share them with us! Our Instagram at
+        @wunderdogsw is one good place to do so. Let’s win the fight against
+        Covid-19 together!
       </p>
     </Chapter>
     </Fragment>
