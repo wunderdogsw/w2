2e24620326806fe8fede0fb5abf9003db78c604b
isla-hash <maria.stenberg@wunderdog.fi>
2020-04-02 10:01:10 +0300

    layout-edit

--- src/App/contents/pages/CustombuiltSolutionsForYourDigitalChallenges/index.js
+++ src/App/contents/pages/CustombuiltSolutionsForYourDigitalChallenges/index.js
@@ -20,8 +20,7 @@ import './index.css'
 
 import photo1 from './wunderdog-helsinki.jpg'
 import photo2 from './retail-wunderdog.jpg'
-import photo3 from './code-wunderdog.jpg'
-import photo4 from './wunderdog-bobo.jpg'
+import photo3 from './joanna-wunderdog.jpg'
 
 
 export default () => (
@@ -36,67 +35,27 @@ export default () => (
 
   <Image src={ photo1 } alt="Wunderdog office" indent="right" hero/>
 
-  <Chapter>
-    <p>
-    <i>
-    Our societies are facing uncertain times. The question of the future is
-    popping up in every business conversation, but one thing is clear: digital
-    reliance is the answer. No matter the size or what industry your company
-    belongs to, Wunderdog brings you the best crafted value solution for your
-    digital investment. How? By using design sprints or developing proof of
-    concepts and minimum viable product depending on the need.
-    </i>
-    </p>
-    </Chapter>
-
-    <Chapter>
-    <h2>What is Wunderdog and how do we work?</h2>
-    </Chapter>
-    <Chapter>
-    <p>
-      Wunderdog is a software development consultancy prioritizing agility ahead
-      of process. We are community players: we first listen to your needs and
-      we offer optimal expert value stream and project value stream accordingly.
-    </p>
-
-    </Chapter>
-
-    <Chapter>
-    <h2>Why are we your perfect match?</h2>
-    </Chapter>
-    <Chapter>
-    <p>
-      We work only with highly skilled senior developers with +5 years of
-      demonstrated technical experience. Since 2014 we have been working with
-      multiple languages, technologies and tools in a variety of industries,
-      company sizes and projects.
+      <Chapter>
       <p>
-      <b>Want some examples? Check a selection of our cases below.</b>
+      <i>
+      Our societies are facing uncertain times. The question of the future is
+      popping up in every business conversation, but one thing is clear: digital
+      reliance is the answer. No matter the size or what industry your company
+      belongs to, Wunderdog brings you the best crafted value solution for your
+      digital investment. How? By using design sprints or developing proof of
+      concepts and minimum viable product depending on the need.
+      </i>
       </p>
-    </p>
-    </Chapter>
+      </Chapter>
 
-    <Chapter>
-    <h2>Do you offer remote consultation?</h2>
-    </Chapter>
-    <Chapter>
-    <p>
-      Yes, we do. When we don’t work at the clients’ facilities, we go the extra
-      mile using the latest remote techniques and functional communication
-      strategies.
-    </p>
-    </Chapter>
+      <Chapter>
+      <h2>We drive you to the brightest future</h2>
+      </Chapter>
 
-    <Chapter>
-    <h2>In which language do you operate?</h2>
-    </Chapter>
-    <Chapter>
-    <p>
-      We speak German and English. Other languages might be available depending
-      on the request.
-      </p>
+      <Chapter>
       <p>
-      <b>We drive you to the brightest future.</b> Contact us for consultancy <a className="noHyphen" href="mailto:jasper.vanghemen@wunderdog.fi">jasper.vanghemen<span>@</span>wunderdog.fi </a>
+      <b>Want some examples of our work? Check a selection of our cases below. </b>
+       Contact us for consultancy <a className="noHyphen" href="mailto:jasper.vanghemen@wunderdog.fi">jasper.vanghemen<span>@</span>wunderdog.fi </a>
       or fill in your e-mail below and let's book a 30 minute phone call for
       ideation (free of charge)!
       </p>
@@ -113,6 +72,7 @@ export default () => (
       <Chapter>
       <h2>RETAIL - Scale your business with us.</h2>
       </Chapter>
+
       <Chapter>
       <p>
       Reaching out to new customers needs tech-savvy hands. We helped one of the
@@ -121,8 +81,8 @@ export default () => (
       city-based service (Berlin) to a European-wide level.
       <TransitionLink to='https://wunderdog.fi/work/fashion-ecommerce'>Read the full case study
       here.</TransitionLink>
-    </p>
-  </Chapter>
+      </p>
+      </Chapter>
 
   <Indent twice><Stats>
     <div>
@@ -133,7 +93,7 @@ export default () => (
     </div>
     </Stats></Indent>
 
-  <Chapter>
+    <Chapter>
     <h2>
       MOBILITY - We accompany you in your structural changes.
     </h2>
@@ -152,12 +112,13 @@ export default () => (
         HR - We help you optimize your teams by hiring better.
       </h2>
       <p>
-        Proper hiring in the tech department can be a difficult task depending
-        on your company size, needs and technical expertise. We offer consulting
-        to startups wanting to build better software architecture and we build
-        the best technical teams to ensure the continuity of the projects.
-        </p>
+      Proper hiring in the tech department can be a difficult task depending
+      on your company size, needs and technical expertise. We offer consulting
+      to startups wanting to build better software architecture and we build
+      the best technical teams to ensure the continuity of the projects.
+      </p>
       </Chapter>
+
     <Indent twice><Stats>
       <div>
         <span>90%</span>
@@ -171,6 +132,7 @@ export default () => (
         </span>
       </div>
       </Stats></Indent>
+
       <Chapter>
         <h2>
           SOCIAL PROJECTS - We improve your performance to help your communities
@@ -187,6 +149,7 @@ export default () => (
         Wunderdog contributed its part that lead to:
         </p>
         </Chapter>
+
       <Indent twice><Stats>
         <div>
           <span>700+</span>
@@ -206,10 +169,58 @@ export default () => (
         </Stats></Indent>
 
         <Chapter>
-        <h2>
-      We're your partner in turning good ideas into digital reality
-      </h2>
-      </Chapter>
+        <h2>What is Wunderdog and how do we work?</h2>
+        </Chapter>
+
+        <Chapter>
+        <p>
+          Wunderdog is a software development consultancy prioritizing agility ahead
+          of process. We are community players: we first listen to your needs and
+          we offer optimal expert value stream and project value stream accordingly.
+        </p>
+        </Chapter>
+
+        <Chapter>
+        <h2>Why are we your perfect match?</h2>
+        </Chapter>
+
+          <Chapter>
+          <p>
+          We work only with highly skilled senior developers with +5 years of
+          demonstrated technical experience. Since 2014 we have been working with
+          multiple languages, technologies and tools in a variety of industries,
+          company sizes and projects.
+          </p>
+          </Chapter>
+
+          <Chapter>
+          <h2>Do you offer remote consultation?</h2>
+          </Chapter>
+
+          <Chapter>
+          <p>
+          Yes, we do. When we don’t work at the clients’ facilities, we go the extra
+          mile using the latest remote techniques and functional communication
+          strategies.
+          </p>
+          </Chapter>
+
+          <Chapter>
+          <h2>In which language do you operate?</h2>
+          </Chapter>
+
+          <Chapter>
+          <p>
+          We speak German and English. Other languages might be available depending
+          on the request.
+          </p>
+          </Chapter>
+
+          <Chapter>
+          <h2>
+          We're your partner in turning good ideas into digital reality
+          </h2>
+          </Chapter>
 
   <Indent twice>
     <Logos>
@@ -228,7 +239,7 @@ export default () => (
     <p><b>Scoped projects & co-sourcing</b><br />Reliable delivery to accelerate the development process</p>
   </Chapter>
 
-          <Image src={ photo4 } alt="Wunderdog sales" indent="left" hero/>
+          <Image src={ photo3 } alt="Wunderdog sales" indent="left" hero/>
 
 
           <Chapter>
