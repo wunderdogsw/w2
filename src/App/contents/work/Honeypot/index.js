cd0d09cd5f71e50b636fe7f9ca26d4de1ce97c80
jonaskay <jonaskay@iki.fi>
2020-07-08 08:56:33 +0300

    Fix date

--- src/App/contents/work/Honeypot/index.js
+++ src/App/contents/work/Honeypot/index.js
@@ -4,7 +4,7 @@ import image from './HoneypotCult-Wunderdog1.jpg'
 export default {
   title: 'Giving content managers full ownership with Contentful',
   client: 'Honeypot',
-  publishDate: 'July 9, 2020',
+  publishDate: 'July 8, 2020',
   keywords: 'scrum',
   category: 'consumers, media',
   image: image,
