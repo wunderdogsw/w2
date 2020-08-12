d120bfe364d38aef811966079140095a9730decd
isla-hash <maria.stenberg@wunderdog.fi>
2020-08-12 10:04:25 +0300

    first version, photos still need editing

--- src/App/contents/work/Smartum/index.js
+++ src/App/contents/work/Smartum/index.js
@@ -1,12 +1,12 @@
 import importedComponent from 'react-imported-component'
-import image from './FinBB17.jpg'
+import image from './smartum-wunderdog.jpg'
 
 export default {
-  title: 'Increasing the number of consenting adults',
-  client: 'FinBB',
-  publishDate: 'August 11, 2020',
+  title: 'Adding more benefits to employee benefits',
+  client: 'Smartum',
+  publishDate: 'August 12, 2020',
   keywords: 'scrum, full-stack development',
-  category: 'healthcare',
+  category: 'consumers',
   image: image,
 
   content: importedComponent(() => import('./content')),
